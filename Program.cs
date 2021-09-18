fc27324ab55c2cb5acb82dc0811b7963918fedba
AplyQ8 <kirill.smith.98@inbox.ru>
2021-09-18 20:50:34 +0300

    Задание 1.1

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    public class StringList
+    {
+        private string[] STR = new string[100];
+        private int temp = 0;
+        public int Insert(string ss)
+        {
+            
+            if(temp > 100)
+            {
+                Console.WriteLine("Вы вышли за границы массива");
+            }
+            else
+            {
+                STR[temp] = ss;
+                temp++;
+            }
+            return 0;
+        }
+        public void Delete(int D_index)
+        {
+            string[] newSTR = new string[STR.Length];
+
+            for(int i=0; i<D_index; ++i)
+                newSTR[i] = STR[i];
+
+            for (int i = D_index+1; i < STR.Length; ++i)
+                newSTR[i-1] = STR[i];
+            temp--;
+
+            STR = newSTR;
+        }
+        public int Search( string ss)
+        {
+            int RightIndex =-1;
+            for(int i=0; i<STR.Length; ++i)
+            {
+                if (STR[i] == ss)
+                {
+                    RightIndex = i;
+                    break;
+                }
+                if (i == STR.Length - 1)
+                    Console.WriteLine("Данное слово не найдено");
+            }
+            return RightIndex;
+        }
+        public void Update (string ss, int index)
+        {
+            string[] newSTR = new string[STR.Length];
+            for (int i = 0; i < index; ++i)
+                newSTR[i] = STR[i];
+            for (int i = index + 1; i < STR.Length; ++i)
+                newSTR[i] = STR[i];
+            newSTR[index] = ss;
+            STR = newSTR;
+        }
+        public string GetAt(int i)
+        {
+
+            return STR[i];
+        }
+        public static void Main(string[] argc)
+        {
+            StringList obj1 = new StringList();
+        }
+    }
+}
