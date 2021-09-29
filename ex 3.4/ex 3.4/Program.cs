a6ffd86b840cf1fc149a1b7eb0417c638bd8fc46
nikolaivrzhesinsky <63013424+nikolaivrzhesinsky@users.noreply.github.com>
2021-09-29 15:56:17 +0300

    -
    -

---
+++ ex 3.4/ex 3.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex_3._4
+{
+    class paramsTest
+    {
+        public static int sum(params int[] array)
+        {
+            int Sum = 0;
+            foreach(int temp in array)
+            {
+                Sum += temp;
+            }
+            return Sum;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(paramsTest.sum(25, 4, 4)  );
+        }
+    }
+}
