62b287d40f6e6d0cb98af06d08342dc9d50cb311
nikolaivrzhesinsky <63013424+nikolaivrzhesinsky@users.noreply.github.com>
2021-10-01 12:22:13 +0300

    Create ex 3.5.cs

---
+++ ex 3.5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task3_5
+{
+	static class Obmennik
+	{
+		public static void Obmen(ref int a, ref int b)
+		{
+			(b, a) = (a, b);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 15;
+			int b = 45;
+
+			Console.WriteLine("do");
+			Console.WriteLine("a = {0}, b = {1}", a, b);
+
+			Obmennik.Obmen(ref a, ref b);
+			Console.WriteLine("posle");
+			Console.WriteLine("a = {0}, b = {1}", a, b);
+		}
+	}
+}
