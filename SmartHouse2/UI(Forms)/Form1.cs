e94d1047cf47a3b99bdfa232fdb5952f145ec817
AplyQ8 <kirill.smith.98@inbox.ru>
2021-10-26 23:01:07 +0300

    it works

---
+++ SmartHouse2/UI(Forms)/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmartHouseLibrary;
+
+namespace UI_Forms_
+{
+    public partial class Form1 : Form
+    {
+        public BuisnessLogic bl = new BuisnessLogic();
+        
+        public Form1()
+        {
+            bl.DataCreation();
+            bl.Update();
+            InitializeComponent();
+        }
+
+        
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Print_Click(object sender, EventArgs e)
+        {
+            PrintBox.Clear();
+            PrintBox.Text = bl.Print();
+        }
+
+        private void PrintBox_TextChanged(object sender, EventArgs e)
+        {
+            PrintBox.ScrollBars = ScrollBars.Vertical;
+        }
+
+        private void PrintLine_Click(object sender, EventArgs e)
+        {
+            PrintBox.Clear();
+            PrintLineForm plf = new PrintLineForm();
+            plf.Show(this);
+            
+            
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            PrintBox.Clear();
+            
+        }
+    }
+}
---
+++ SmartHouse2/UI(Forms)/PrintLineForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using SmartHouseLibrary;
+
+namespace UI_Forms_
+{
+    public partial class PrintLineForm : Form
+    {
+        BuisnessLogic bl = new BuisnessLogic();
+        public PrintLineForm()
+        {
+            
+            InitializeComponent();
+        }
+        
+        private void ok_button_Click(object sender, EventArgs e)
+        {
+            Form1 F1 = (Form1)this.Owner;
+            int x = LineNumberBox.Text.ParseInt(1);
+            F1.PrintBox.Text = F1.bl.PrintLine(x);
+            this.Close();
+        }
+    }
+}
